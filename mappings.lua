1507e75af0704a1602a77ab4888f198cf9f3ae5c
miocodin <phieu@duck.com>
2023-07-11 10:09:23 +0700

    compile and run c+++ file key && add snippets

--- mappings.lua
+++ mappings.lua
@@ -6,6 +6,19 @@
 return {
   -- first key is the mode
   n = {
+    ["<F9>"] = { 
+      ":w <bar> :vsplit <bar> !g++ -std=c++17 -Wall -Wextra -Wpedantic % -o %:r <cr> :terminal %:r <cr>", 
+      desc = "Compile and Run cpp file" 
+    },
+    ["<F8>"] = { 
+      ":w <bar> !g++ -std=c++17 -Wall -Wextra -Wpedantic % -o %:r <cr>",
+      desc = "Compile cpp file only"
+    },
+    ["<F10>"] = {
+      ":w <bar> :vsplit <bar> :terminal %:r <cr>",
+      desc = "Run cpp file only"
+    },
+
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
